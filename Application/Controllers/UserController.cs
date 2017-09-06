728d27dcd234563fb462e60472087eda298405cd
Senthilnathan Kirisanth <senthilnathan_k@3logixs.lk>
2017-09-06 18:29:38 +0530

    First commit

---
+++ Application/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Application.Models;
+
+namespace Application.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public ActionResult AddOrEdit(int id=0)
+        {
+            ViewBag.Message = "Your Registration page.";
+            user userModel = new user();
+            return View(userModel);
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(user userModel)
+        {
+            using (DbModels dbModel = new DbModels())
+            {
+                if (dbModel.users.Any(x => x.userName == userModel.userName))
+                {
+                    ViewBag.DuplicateMessage = "User Name is Exists !";
+                    return View("AddOrEdit", userModel);
+                }  
+                dbModel.users.Add(userModel);
+                dbModel.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Registration is success";
+            return View("AddOrEdit", new user());
+        }
+    }
+}
